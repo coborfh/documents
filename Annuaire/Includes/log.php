7dc814a58af24e63b6949faa2d29f3f95aea92d7
coborfh <charlesadam.co@gmail.com>
2016-07-29 13:36:49 +0000

    Add files via upload

--- Annuaire/Includes/log.php
+++ Annuaire/Includes/log.php
@@ -1,3 +1,4 @@
+
 <div class="container pat">
     <div class="row">
         <div class="col-md-3 col-md-offset-4">
@@ -15,22 +16,20 @@
     padding: 20px;
     ">
                 
-                <form class="form-signin" action="#">
+                <form class="form-signin" method="post" action="Vues/other.php">
                 <!-- <div class="col-md-10"> -->
                 <div class="form-group">
-                    <input type="text" class="form-control input-md" placeholder="Pseudo" required autofocus />
+                    <input type="text" class="form-control input-md" id="pseudo" name="pseudo" placeholder="Pseudo" required autofocus />
                 </div>
                 <!-- </div> -->
                 <div class="form-group">
-                    <input type="password" class="form-control input-md" placeholder="Password" required />
+                    <input type="password" class="form-control input-md" id="password" name ="password" placeholder="Password" required />
                 </div>
                 <label class="checkbox" style="color: black">
                     <input type="checkbox" value="remember-me" />
                     Se souvenir de moi
                 </label>
-                <button class="btn btn-lg btn-block purple-bg" type="submit" style=" background-color: #6E329D;
-    color: #fff;">
-                    Valider</button>
+                <button class="btn btn-lg btn-block purple-bg" name ="envoyer" type="submit" style=" background-color: #6E329D; color: #fff;">Valider</button>
                 </form>
                 <a class="forgotLnk" href="" style=" margin-top: 10px; display: block;">J'ai oublié mon mot de passe...</a>
                 
